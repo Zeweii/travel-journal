37a66281e3de671594059e61142b88831dc5e2b0
Zachary Jiang <zacharyjiang720@gmail.com>
2022-08-12 00:47:08 +1000

    added title, date, description

--- src/data.js
+++ src/data.js
@@ -1,29 +1,32 @@
 export default [
     {
         id: 1,
-        location: "JAPAN", 
-        title: "title" ,
-        date: "date"  ,
-        description: "desc",
+        location: "Shizuoka, JAPAN", 
+        title: "Mt. Fuji" ,
+        date: "12 Jan, 2021 - 24 Jan, 2021"  ,
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). " +
+        "Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         mapsLink: "https://goo.gl/maps/RR2NFRiXTV8yMshv7",
         coverImg: "https://source.unsplash.com/WLxQvbMyfas" ,
     },
     {
         id: 2,
-        location: "AUSTRALIA", 
-        title: "title" ,
-        date: "date"  ,
-        description: "desc",
-        mapsLink: "https://goo.gl/maps/RR2NFRiXTV8yMshv7",
+        location: "Sydney, AUSTRALIA", 
+        title: "Sydney Harbour Bridge" ,
+        date: "27 May, 2021 - 8 Jun, 2021"  ,
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. " + 
+        "Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        mapsLink: "https://goo.gl/maps/Q3RWozVmVKMGivLL8",
         coverImg: "https://source.unsplash.com/JmuyB_LibRo" ,
 
     },{
         id: 3,
-        location: "US", 
-        title: "title" ,
-        date: "date"  ,
-        description: "desc",
-        mapsLink: "https://goo.gl/maps/RR2NFRiXTV8yMshv7",
+        location: "Møre og Romsdal, NORWAY", 
+        title: "Geirangerfjord" ,
+        date: "01 Oct, 2021 - 18 Nov, 2021"  ,
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. " + 
+        "It is located entirely in the Stranda Municipality.",
+        mapsLink: "https://goo.gl/maps/8UzqzMaJ2jcU14v96",
         coverImg: "https://source.unsplash.com/3PeSjpLVtLg" ,
 
     }
